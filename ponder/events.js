94d6642bca2d13773ca9e6e713fc0c0aeb618787
Brayden Jones <jon20015@byui.edu>
2025-05-17 14:26:14 -0600

    Ponder W04  - moved css to its own file

--- ponder/events.js
+++ ponder/events.js
@@ -2,14 +2,20 @@
 
 function newTask() {
     // get the list element from the DOM
-    const listElement = document.querySelector("#todo-list");
+    const listElement = document.querySelector("#todoList");
 
     // get the value entered into the #todo input
     const task = document.querySelector("#todo").value;
+    document.querySelector("#todoList").value = "";
+    
+    let first = task.split("-")[0];
+    let second = task.split("-")[1];
+    console.log(first);
+    console.log(second);
 
     // render out the list 
     listElement.innerHTML += `
-        <li> ${task}S
+        <li> ${task}
             <div>
                 <span data-function="delete">❎</span>
                 <span data-function="complete">✅</span>
@@ -18,13 +24,14 @@ function newTask() {
 }
 
 function manageTasks(e) {
+    console.log(e);
     // using the event find the li element closest to what they clicked
     const parent = e.target.closest("li");
     // did they click the delete or complete icon?
     if (e.target.getAttribute('data-function') === "delete") {
       parent.remove();
     }
-    if (e.target.getAttribute('data-function') === "complete") {
+    else if (e.target.getAttribute('data-function') === "complete") {
     parent.classList.toggle('strike');
     }
 }
